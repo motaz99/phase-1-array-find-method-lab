ac85cbbf72cb009889106c64cd60dcd07ef1991a
motaz <motaz.dn.ali@gmail.com>
2023-03-21 13:28:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+
+const superbowlWin = (record) => {
+  const winObj = record.find((element) =>
+    element.result === "W" ? element : undefined
+  );
+  return winObj ? winObj.year : undefined;
+};
